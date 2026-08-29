ed53d557154afb6a3cbd4fbcd4d78c16248884de
vitorourense-glitch <vitorourense@gmail.com>
2026-08-29 20:01:38 +0200

    Update hydra_skidrow_scraper.py

--- hydra_skidrow_scraper.py
+++ hydra_skidrow_scraper.py
@@ -65,4 +65,4 @@ class HydraSkidrowScraper:
 
 if __name__ == "__main__":
     scraper = HydraSkidrowScraper()
-    scraper.scrape_to_hydra_format(pages_to_scrape=40)
+    scraper.scrape_to_hydra_format(pages_to_scrape=6)
